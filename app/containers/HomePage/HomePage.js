68ffcf3e1c62d6d6d97b1ce748eec885440cbb42
rennova <rennova.js@gmail.com>
2018-11-02 16:14:01 +0200

    Cleaning

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -35,27 +35,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <meta name="description" content="A React.js Boilerplate application homepage" />
         </Helmet>
         <div className="home-page">
-          <section className="centered">
-            <h2>Start your next react project in seconds</h2>
-            <p>A minimal <i>React-Redux</i> boilerplate with all the best practices</p>
-          </section>
-          <section>
-            <h2>Try me!</h2>
-            <form onSubmit={this.props.onSubmitForm}>
-              <label htmlFor="username">
-              Show Github repositories by
-                <span className="at-prefix">@</span>
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="flexdinesh"
-                  value={this.props.username}
-                  onChange={this.props.onChangeUsername}
-                />
-              </label>
-            </form>
-            <ReposList {...reposListProps} />
-          </section>
+          
         </div>
       </article>
     );
